49533a604a820447066b77bd3cb2976c940cc761
unknown <90862670+Vadim-kaR@users.noreply.github.com>
2022-04-14 20:14:34 +0300

    second commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallaryContainer = document.querySelector('.gallery');
+const imageListMurkup = createImageListMarkup(galleryItems);
+
+gallaryContainer.innerHTML = imageListMurkup;
+gallaryContainer.addEventListener('click' ,() => onImageClick)
+
+function createImageListMarkup(item) { 
+    return item.map(({ preview, original, description }) =>
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>`).join('')
+    
+}
+
+function onImageClick(e) { 
+    if (e.target.nodeName !== "IMG") { 
+        return;
+    }
+    e.preventDefault();
+}
+
+
+
